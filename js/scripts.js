60977357a401e85fd41ac7c9e77dfe700e16ce4f
nellagio <nickgallo41@gmail.com>
2020-01-17 14:13:39 -0800

    progess made in javascript. submit <button> reloads page now!

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
-var pos = +1;
-var neg = 0;
-var total = pos + neg;
-console.log(total)
-}
-});
+  $(".survey").submit(function() {
+    $(".question-one").click("")
+    $(".question-two").click("")
+    $(".question-three").click
+    $(".question-four").click
+    $(".question-five").click
+
+    $(".html").show("python");
+
+    event.preventDefault();
+  }
+// var ("javascript") = 2 = 3; 
+// var ("python") = 4;
+
+// console.log(total)
+// }
+// });
+
+// $("#formOne").submit(function() {
+//   $(".person1").append("blah blah");
+//   $(".person2").append("blah blah");
+//   $(".animal").append("blah blah");
+//   $(".exclamation").append("blah blah");
+//   $(".verb").append("blah blah");
+//   $(".noun").append("blah blah");
+
+//   $("#story").show();
+// });
+// });
+
+
+// on submit (.click), alert "git ready to code!", show specific language class from the results from total value captured 
