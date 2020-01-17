cdb3e4aff9f04b7d0f5529720b5d9e9c8b73011a
nellagio <nickgallo41@gmail.com>
2020-01-17 13:07:20 -0800

    adjust functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-var pos = 1;
+var pos = +1;
 var neg = 0;
 var total = pos + neg;
-
-if (total >=3)return("python");{
-console.log(total);
+console.log(total)
 }
 });
