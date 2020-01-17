4dff9b782be2caaef82be0055eff0b47a9b02076
nellagio <nickgallo41@gmail.com>
2020-01-17 11:26:05 -0800

    add 5 questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
- var questions = 
- var score = 0;
-   prompt: "What do t"
 
-   for(var i=0; i < questions.length; i++)
-    var response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-      score++;
-      alert("Correct!");
-    }else {
-      alert("wrong!");
-    }
-}
-alert("you got " + score + "/") + questions.length);
+
+
+});
