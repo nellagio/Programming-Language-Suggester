42e015dbef7f0c6f6381696e7484ad8e4bb84b0c
nellagio <nickgallo41@gmail.com>
2020-01-17 15:43:52 -0800

    attempt to understand function with in code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,9 @@
 $(document).ready(function() {
-  $(".survey").submit(function() {
-    $(".question-one").click("")
-    $(".question-two").click("")
-    $(".question-three").click
-    $(".question-four").click
-    $(".question-five").click
-
-    $(".html").show("python");
+  console.log('ready');
+  $(".survey").submit(function(event) {
+    console.log('hello');
 
     event.preventDefault();
   }
-// var ("javascript") = 2 = 3; 
-// var ("python") = 4;
-
-// console.log(total)
-// }
-// });
-
-// $("#formOne").submit(function() {
-//   $(".person1").append("blah blah");
-//   $(".person2").append("blah blah");
-//   $(".animal").append("blah blah");
-//   $(".exclamation").append("blah blah");
-//   $(".verb").append("blah blah");
-//   $(".noun").append("blah blah");
-
-//   $("#story").show();
-// });
-// });
-
-
-// on submit (.click), alert "git ready to code!", show specific language class from the results from total value captured 
+  });
+// on submit (.click), alert "git ready to code!", show specific language class from the results from total value captured} 
